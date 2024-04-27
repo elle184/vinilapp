8e27dfa6b5efe2965570917757034ff581b5bfbc
Adrian Becerra <a.becerraa@uniandes.edu.co>
2024-04-27 16:34:49 -0500

    Mejoras en el diseño del menú inicial (#58)
    * Avances en el diseño del menú principal
    
    * Se mejora el diseño de la pantalla inicial.
    
    * Mejoras en el diseño del menú inicial.
    
    * Mejoras en el diseño y la navegación del sitio.

--- VinilApp/app/build.gradle.kts
+++ VinilApp/app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -46,6 +47,9 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
+    }
+    dataBinding {
+        enable = true
     }
 }
 
@@ -59,6 +63,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
