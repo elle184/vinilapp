fb88de85629aae07a2238a16f8105881e5c4d22d
Adrián Francisco Becerra Arias <a.becerraa@uniandes.edu.co>
2024-04-27 13:46:45 -0500

    Mejoras en el diseño y la navegación del sitio.

--- VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/MainActivity.kt
+++ VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/MainActivity.kt
@@ -4,21 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import edu.co.grupo4.vinilapp.ui.theme.VinilAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InitialMenu()
+                    Home()
                 }
             }
         }
@@ -39,7 +51,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MusicCollector(modifier : Modifier = Modifier) {
+fun Home() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "MainActivity") {
+        composable("MainActivity") { InitialMenu(navController) }
+        composable("OptionsMenu") { OptionsMenu() }
+    }
+}
+
+@Composable
+fun MusicCollector(
+    modifier : Modifier = Modifier) {
     val image = painterResource(R.drawable.collector)
     
     Box(modifier) {
@@ -51,7 +74,8 @@ fun MusicCollector(modifier : Modifier = Modifier) {
 }
 
 @Composable
-fun Visitor(modifier : Modifier = Modifier) {
+fun Visitor(
+    modifier : Modifier = Modifier) {
     val image = painterResource(R.drawable.guest)
 
     Box(modifier) {
@@ -71,36 +95,73 @@ fun Header(modifier : Modifier = Modifier) {
             painter = image
             , contentDescription = null
         )
-        Text(text = "VinilApp")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InitialMenu(
+    navController : NavHostController
+    , modifier : Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                    , titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                title = {
+                    Text(
+                        text = "VinilApp"
+                        , style = MaterialTheme.typography.bodyLarge)
+                }, navigationIcon = {
+                    Header(modifier = Modifier.size(60.dp))
+                }
+            )
+        }
+    ) {
+        innerPadding -> Column(
+            modifier = Modifier.padding(innerPadding)
+            , verticalArrangement = Arrangement.Center
+        ) {
+            Column (
+                verticalArrangement = Arrangement.Center
+                , modifier = modifier
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween
+                    , modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    ImageWithNavigation(
+                        painter = painterResource(id = R.drawable.collector)
+                        , onClick = { navController.navigate("OptionsMenu") })
+                    ImageWithNavigation(
+                        painter = painterResource(id = R.drawable.guest)
+                        , onClick = { navController.navigate("OptionsMenu") })
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun InitialMenu(modifier : Modifier = Modifier) {
-    Column (
-        verticalArrangement = Arrangement.SpaceAround
-        , modifier = modifier
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween
-            , modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Header(modifier = modifier)
-        }
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween
-            , modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            MusicCollector(modifier = Modifier.padding(end = 30.dp))
-            Visitor(modifier = Modifier.padding(start = 30.dp))
-        }
-    }
+fun ImageWithNavigation(painter : Painter, onClick : () -> Unit) {
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = Modifier
+            .size(150.dp)
+            .clickable { onClick() }
+            .padding(8.dp),
+        alignment = Alignment.Center,
+        contentScale = androidx.compose.ui.layout.ContentScale.Crop,
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VinilAppTheme {
-        InitialMenu()
+        Home()
     }
 }
--- VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/ui/theme/Type.kt
+++ VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/ui/theme/Type.kt
@@ -2,33 +2,37 @@ package edu.co.grupo4.vinilapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import edu.co.grupo4.vinilapp.R
+
+var Montserrat = FontFamily(
+    Font(R.font.montserrat_regular)
+    , Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
