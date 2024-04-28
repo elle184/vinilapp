edef737547019ed1221a768ab44a63c854e7c9a4
Adrian Becerra <a.becerraa@uniandes.edu.co>
2024-04-28 18:28:04 -0500

    HU16 - Menú principal (#60)
    * Avances en el diseño del menú principal
    
    * Se mejora el diseño de la pantalla inicial.
    
    * Mejoras en el diseño del menú inicial.
    
    * Scaffold
    
    * Codigo para listar coleccionistas
    
    * Limpieza de código fuente.
    
    * Cambio en el nombramiento de los endpoints.
    
    ---------
    
    Co-authored-by: jdac <jdac96@gmail.com>
    Co-authored-by: MariaScoth <mariascoth@gmail.com>
    Co-authored-by: Adrián Francisco Becerra Arias <lucka@Adrians-MacBook-Pro.local>

--- VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/model/Collector.kt
+++ VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/model/Collector.kt
@@ -1,8 +1,8 @@
 package edu.co.grupo4.vinilapp.model
 
 data class Collector(
-    val id : Int
-    , val name : String
-    , val telephone : String
-    , val email : String
+    val collectorId: Int,
+    val name:String,
+    val telephone:String,
+    val email:String
 )
