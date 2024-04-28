f4c19a605ba702b770790880dd550df18e72d635
MariaScoth <mariascoth@gmail.com>
2024-04-28 17:03:27 -0500

    Codigo para listar coleccionistas

---
+++ VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/adapters/CollectorsAdapter.kt
@@ -0,0 +1,56 @@
+package edu.co.grupo4.vinilapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import edu.co.grupo4.vinilapp.R
+import edu.co.grupo4.vinilapp.databinding.CollectorItemBinding
+import edu.co.grupo4.vinilapp.model.Collector
+
+
+class CollectorsAdapter : RecyclerView.Adapter<CollectorsAdapter.CollectorViewHolder>(){
+
+    var collectors :List<Collector> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectorViewHolder {
+        val withDataBinding: CollectorItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            CollectorViewHolder.LAYOUT,
+            parent,
+            false)
+        return CollectorViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: CollectorViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.collector = collectors[position]
+        }
+        holder.viewDataBinding.root.setOnClickListener {
+            val action = CollectorFragmentDirections.actionCollectorFragmentToAlbumFragment()
+            // Navigate using that action
+            holder.viewDataBinding.root.findNavController().navigate(action)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return collectors.size
+    }
+
+
+    class CollectorViewHolder(val viewDataBinding: CollectorItemBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.collector_item
+        }
+    }
+
+
+}
---
+++ VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/network/NetworkServiceAdapter.kt
@@ -0,0 +1,66 @@
+package edu.co.grupo4.vinilapp.network
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import edu.co.grupo4.vinilapp.models.Collector
+import org.json.JSONArray
+import org.json.JSONObject
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.android.volley.RequestQueue
+
+
+class NetworkServiceAdapter constructor(context: Context) {
+    companion object{
+        const val BASE_URL= "https://vynils-back-heroku.herokuapp.com/"
+        var instance: NetworkServiceAdapter? = null
+        fun getInstance(context: Context) =
+            instance ?: synchronized(this) {
+                instance ?: NetworkServiceAdapter(context).also {
+                    instance = it
+                }
+            }
+    }
+    private val requestQueue: RequestQueue by lazy {
+        // applicationContext keeps you from leaking the Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+
+    fun getCollectors(onComplete:(resp:List<Collector>)->Unit, onError: (error:VolleyError)->Unit) {
+        requestQueue.add(getRequest("collectors",
+            Response.Listener<String> { response ->
+                Log.d("tagb", response)
+                val resp = JSONArray(response)
+                val list = mutableListOf<Collector>()
+                for (i in 0 until resp.length()) {
+                    val item = resp.getJSONObject(i)
+                    list.add(i, Collector(collectorId = item.getInt("id"),name = item.getString("name"), telephone = item.getString("telephone"), email = item.getString("email")))
+                }
+                onComplete(list)
+            },
+            Response.ErrorListener {
+                onError(it)
+                Log.d("", it.message.toString())
+            }))
+    }
+    
+    private fun getRequest(path:String, responseListener: Response.Listener<String>, errorListener: Response.ErrorListener): StringRequest {
+        return StringRequest(Request.Method.GET, BASE_URL+path, responseListener,errorListener)
+    }
+    private fun postRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+        return  JsonObjectRequest(Request.Method.POST, BASE_URL+path, body, responseListener, errorListener)
+    }
+    private fun putRequest(path: String, body: JSONObject, responseListener: Response.Listener<JSONObject>, errorListener: Response.ErrorListener ):JsonObjectRequest{
+        return  JsonObjectRequest(Request.Method.PUT, BASE_URL+path, body, responseListener, errorListener)
+    }
+
+}
+
+
+
+
+
