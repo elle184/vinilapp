afa7191928a9bf138a7da5a78a1b60b5227c9620
MariaScoth <mariascoth@gmail.com>
2024-05-05 22:37:12 -0500

    Se sube avance de las actividades de las listas de coleccionistas

---
+++ VinilApp/app/src/main/java/edu/co/grupo4/vinilapp/ListaColeccionistaActivity.kt
@@ -0,0 +1,20 @@
+package edu.co.grupo4.vinilapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListaColeccionistaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lista_coleccionista)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
