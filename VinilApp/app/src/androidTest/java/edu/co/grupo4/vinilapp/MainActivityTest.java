14dc0964bde713c10a775ae5d9e89116aa3c501a
NataliaRojas <np.rojas1158@uniandes.edu.co>
2024-05-26 12:22:59 -0500

    Modificaciones para mejorar accesibilidad

--- VinilApp/app/src/androidTest/java/edu/co/grupo4/vinilapp/MainActivityTest.java
+++ VinilApp/app/src/androidTest/java/edu/co/grupo4/vinilapp/MainActivityTest.java
@@ -4,7 +4,6 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.hamcrest.Matchers.allOf;
 
